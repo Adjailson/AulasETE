37b3c0b581c449e5bfa06941b0797a150e378ac6
Adjailson F. Melo <faculdade.upe@gmail.com>
2021-09-27 21:52:23 -0300

    Aula do dia 27/set/2021
    Semáforo finalizado...

--- semaforo/semaforo.py
+++ semaforo/semaforo.py
@@ -1,19 +1,30 @@
 import tkinter as tk
 import time as t
-#import threading
+import threading
 
-cont = 0
-while True:
-    print("Olá...")
-    t.sleep(5)
+def iniciar():
+    while True:
+        label['image'] = amarelo
+        t.sleep(2)
+        label['image'] = vermelho
+        t.sleep(2)
+        label['image'] = verde
+        t.sleep(2)
 
 janela = tk.Tk()
 f = tk.Frame(janela)
 
+amarelo = tk.PhotoImage(file='amarelo.png')
+vermelho = tk.PhotoImage(file='vermelho.png')
+verde = tk.PhotoImage(file='verde.png')
+
+label = tk.Label(f,image=amarelo)
+#label.bind("<Button-1>",iniciar)
+label.grid(row=0, column=0)
 
 
 f.pack()
-#threading.Thread(target=funcao, daemon=True).start()
+threading.Thread(target=iniciar, daemon=True).start()
 janela.mainloop()
         
     
